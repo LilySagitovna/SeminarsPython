544ba589c6055de96795e7930f963bff475c7ae9
Lily <ms.lilichka@yandex.ru>
2022-09-09 19:05:38 +0500

    Lection One

---
+++ FerstLectionPython/hello.py
@@ -0,0 +1,79 @@
+# 
+# тип данных и переменная
+# int, float, boolean, list,str,None
+value = None
+# print(type(value))
+
+a = 123
+b = 1.23
+value = 12334
+
+# print(a, type(a))
+# print (b, type(b))
+# print(value, type(value))
+
+s = "i 'love you"
+s = 'i \'love you'
+s = "i \nlove you"
+# print(s, type(s))
+s = 'i "love you'
+# print(s, type(s)) # комментарии
+
+# print(a, '-', b, '-', s)
+# print('{} - {} - {}'.format(a,b,s))
+# print('{1} - {2} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+f = True
+q = False
+# print(f,q)
+
+list = [1,2,3]
+col = ['trre', 1.77, True]
+# print(list)
+
+#print() # вывести данные пользователю - отвечает за вывод данных
+#input() # взять данные у пользователя - отвечает за ввод данных
+
+# print('Введите v')
+# v = input()
+# print('Введите p')
+# p = input()
+# print(v, '+', p, '=', v + p)
+
+# print('Введите i')
+# i = int(input())
+# print('Введите y')
+# y = int(input())
+# print(i, '+', y, '=', i + y)
+
+#Арифметические операции
+# a=123
+# b=-321
+# c= a+b
+# print(c)
+
+# a=12
+# b=5
+# c= a//b #деление,с выводом целого числа, / - дробное выведит
+# print(c)
+
+# a=12
+# b=8
+# c= a%b # остаток от деления
+# print(c)
+
+# a=12
+# b=5
+# # c= a/b # дробное
+# # print(c)
+
+# с=a ** b # возведение в степень
+# print(с)
+
+# a=1.3458648
+# b=2
+# c=round(a*b,3)
+# print(c)
+
+a = int(input('a='))
